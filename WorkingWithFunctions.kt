43c559fa06b545a1209e38ea238ae6415e8640e3
Clinton-Ogada <91396750+Clinton-Ogada@users.noreply.github.com>
2023-02-06 17:59:02 +0300

    Create WorkingWithFunctions.kt
    Updated a program created using a function

---
+++ WorkingWithFunctions.kt
@@ -0,0 +1,20 @@
+fun main() {
+    mydata()
+    
+}
+
+fun mydata() {
+  //typealias  var name="Clinton"
+   var age=50
+  // println("Welcome ${name}. You are ${age} years old")
+   
+if(age >10 && age<18){
+    println("Junior")
+}
+    else if(age >18 && age<100){
+    println("Senior")
+}
+else{
+    println("Baby")
+}
+}
